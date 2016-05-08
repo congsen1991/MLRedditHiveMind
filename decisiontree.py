732a73bf4f3d4bc4acb1b09c2ea5b14056e72de6
congsen1991 <congsen1991@gmail.com>
2016-05-08 17:35:45 -0400

    Added files via upload

--- decisiontree.py
+++ decisiontree.py
@@ -21,22 +21,18 @@ from sklearn.metrics import confusion_matrix
 from sklearn.linear_model import LogisticRegression
 from sklearn.datasets import load_iris
 from sklearn.feature_selection import SelectFromModel
+
+#scale the score into binary value
 def convertScore(row,minVal,maxVal):
     score = row['score']
-    if score<0: return 1
-    if score<10: return 2
-    if score<100: return 3
-    if score<1000: return 4
-    return 5
+    if score<10: return 0
+    return 1
+    
 def readData(subject):
     df = pd.read_pickle(subject + '.pickle')
-    #df.to_csv('../data/' + subject + '.csv', sep=',', encoding='utf-8' )
+    
 for file in glob.glob("processedData/*Data.pickle"):
     subject=("../processData/" + file)[15:-7]
-
-    print subject
-    # cardataframe = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data',header=None)
-    #americaDataFrame = decisionTree("americaData.pickle");
     americaDataFrame = pd.read_pickle(subject+ '.pickle')
     maxVal = americaDataFrame['score'].max()
     minVal = americaDataFrame['score'].min()
@@ -45,24 +41,10 @@ for file in glob.glob("processedData/*Data.pickle"):
     print americaDataFrame.head()
     americaDataFrame.columns = ['body', 'score', 'overallpol','stdPol', 'overallSub', 'stdSub', 'polRange', 'subRange', 'wordCount', 'bigWords', 'sentLen', 'targetPol', 'targetSub','newScore']
     Y = americaDataFrame['newScore'].values
-    X = americaDataFrame[['overallpol','stdPol', 'overallSub', 'stdSub', 'polRange', 'subRange', 'wordCount', 'bigWords', 'sentLen', 'targetPol', 'targetSub']].values
-    print Y
+    X = americaDataFrame[['overallpol', 'overallSub', 'stdSub','sentLen']].values
 
-    # xSet = [2,3,4,5,6,7,8,9,10,11,12]
-    # y=1
-    # X = americaDataFrame.ix[:,xSet]
-    # Y = americaDataFrame.ix[:,y]
-    X = np.array(X).astype(float)
-    Y = np.array(Y).astype(float)
-    print X, Y
-    #sc = StandardScaler()
-    ##sc=sc.fit(X)
-    #X = sc.transform(X)
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=0)
-    # sc = StandardScaler()
-    # sc=sc.fit(X_train)
-    # X_train = sc.transform(X_train)
-    # X_test = sc.transform(X_test)
+
 
     tree = DecisionTreeClassifier(criterion = 'entropy', max_depth=3, random_state=0)
     selector = RFE(tree, 2, step=1)
@@ -70,8 +52,12 @@ for file in glob.glob("processedData/*Data.pickle"):
     selector.support_
     selector.ranking_
     tree.fit(X_train, Y_train)
+    Y_pred=tree.predict(X_test)
+    print('Accuracy: %.2f' % accuracy_score(Y_test,Y_pred))
+        
+    confmat=confusion_matrix(y_true=Y_test, y_pred=Y_pred)
+    print(confmat)
     export_graphviz(tree, out_file= subject + '.dot',feature_names=['overallpol','stdPol', 'overallSub', 'stdSub', 'polRange', 'subRange', 'wordCount', 'bigWords', 'sentLen', 'targetPol', 'targetSub'])
-           #  self.X=self.data.ix[:,xSet]
-    #         self.Y=self.data.ix[:,y]
+
 
 
